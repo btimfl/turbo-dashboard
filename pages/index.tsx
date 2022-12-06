38fa92e39d4821effe3569259a85fff641c48dc6
Raghav <raghav.kanwal@unicommerce.com>
2022-12-06 13:29:50 +0530

    Reset Turbo Dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,15 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import MerchantWindow from './components/MerchantWindow/MerchantWindow'
 
 export default function HomeModule() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Turbo Merchant</title>
+        <title>Turbo Dashboard</title>
         <meta name="description" content="Turbo Merchant Experience" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <MerchantWindow></MerchantWindow>
     </div>
   )
 }
