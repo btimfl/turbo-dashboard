a2b4ca95e494c238f5bbc18cf62ed67c525913cb
Raghav <raghav.kanwal@unicommerce.com>
2022-12-16 15:05:11 +0530

    Basic Table + Flyout Modal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import Navigation from '../components/Navigation/Navigation'
 import Head from 'next/head'
 import HeaderBar from '../components/HeaderBar/HeaderBar'
 import { useState } from 'react'
 import TurboLayout from '../layouts/TurboLayout'
+import { modalTheme } from '../components/theme/flyoutModal'
+
+const theme = extendTheme({
+  components: {
+    Modal: modalTheme
+  }
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   
   return (
-    <ChakraProvider>
+    <ChakraProvider theme = {theme}>
       <TurboLayout>
         <Component {...pageProps} />
       </TurboLayout>
--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,3 +1,149 @@
-export default function DashboardPage() {
-    return <h1>Dashboard Page</h1>
+import { Box, Button, Flex, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from '@chakra-ui/react';
+import { useReactTable, createColumnHelper, getCoreRowModel, flexRender, Table } from '@tanstack/react-table';
+import { useState } from 'react';
+import UserForm from '../../components/HeaderBar/UserForm/UserForm';
+import styles from './users.module.scss';
+
+interface User {
+    name: string,
+    email: string,
+    phone: string,
+    role: string,
+    status: string
+    action?: any
+}
+
+
+
+const columnHelper = createColumnHelper<User>();
+
+
+
+const defaultData: User[] = [
+    {
+        name: 'Raghav Kanwal',
+        email: 'raghav.kanwal@unicommerce.com',
+        phone: '+91 9654723413',
+        role: 'Tech Lead',
+        status: 'Active',
+        action: ''
+    }
+];
+
+export default function UsersPage() {
+
+    const columns = [
+        columnHelper.accessor('name', {
+            header: () => <span className={styles.columnHeader}>Name</span>,
+            cell: info => info.getValue()
+        }),
+        columnHelper.accessor('email', {
+            header: () => <span className={styles.columnHeader}>Email</span>,
+            cell: info => info.getValue()
+        }),
+        columnHelper.accessor('phone', {
+            header: () => <span className={styles.columnHeader}>Phone</span>,
+            cell: info => info.getValue()
+        }),
+        columnHelper.accessor('role', {
+            header: () => <span className={styles.columnHeader}>Role</span>,
+            cell: info => info.getValue()
+        }),
+        columnHelper.accessor('status', {
+            header: () => <span className={styles.columnHeader}>Status</span>,
+            cell: info => info.getValue()
+        }),
+        columnHelper.accessor('action', {
+            header: () => <span className={styles.columnHeader}>Actions</span>,
+            cell: props => { return (<Button size={`xs`} onClick={() => handleEditUser(props)}>Edit</Button>)}
+        }),
+    ]
+
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+    const [data, setData] = useState(() => [...defaultData])
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel()
+    });
+
+
+    const handleEditUser = ({...props}) => {  
+        console.log("Editing user", props?.row?.original);
+        return onOpen()
+    }
+
+
+    return (
+        <>
+            <table className={styles.table}>
+                <thead>
+                    {table.getHeaderGroups().map(headerGroup => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map(header => (
+                                <th key={header.id} >
+                                    <Box p={2} textAlign={`left`}>
+                                    {
+                                        header.isPlaceholder
+                                        ? null
+                                        : flexRender(
+                                            header.column.columnDef.header,
+                                            header.getContext()
+                                        )
+                                    }
+                                    </Box>
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody>
+                    {table.getRowModel().rows.map(row => (
+                        <tr key={row.id}>
+                            {row.getVisibleCells().map(cell => (
+                                <td key={cell.id}>
+                                    <Box className={styles.cellContainer} p={2}>
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </Box>
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+                <tfoot>
+                    {table.getFooterGroups().map(footerGroup => (
+                        <tr key={footerGroup.id}>
+                            {footerGroup.headers.map(header => (
+                                <th key={header.id}>
+                                    {header.isPlaceholder
+                                        ? null
+                                        : flexRender(
+                                            header.column.columnDef.footer,
+                                            header.getContext()
+                                        )}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </tfoot>
+            </table>
+
+            <Modal variant={`flyout`} isOpen={isOpen} onClose={onClose} size={`full`} motionPreset={`none`}>
+                <ModalOverlay />
+                <ModalContent>
+                <ModalHeader fontSize={`md`}>Edit User</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    <UserForm />
+                </ModalBody>
+
+                <ModalFooter justifyContent={`flex-start`}>
+                    <Button mr={3} onClick={onClose} size={`sm`}>Close</Button>
+                </ModalFooter>
+                </ModalContent>
+            </Modal>
+            </>
+    )
 }
