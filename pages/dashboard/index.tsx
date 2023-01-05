5d13e72f876af3e8674f2acf0e49bd83e3a33507
Utkarsh Saxena <utkarsh.saxena@unicommerce.com>
2023-01-05 18:00:17 +0530

    Add layout for dashboard page

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,5 +1,32 @@
+import { Box, Button, Flex, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import styles from "./dashboard.module.scss";
+import { AiFillCaretDown } from "react-icons/ai";
+import { useState } from "react";
+
+enum Duration {
+    LAST_WEEK = "Last 7 Days",
+    LAST_MONTH = "Last Month",
+    LAST_90_DAYS = "Last 90 Days",
+    THIS_WEEK = "This Week",
+    THIS_MONTH = "This Month",
+}
+
 export default function Dashboard() {
-    return <h1>Dashboard Page</h1>
+    const [duration, setDuration] = useState<string>(Duration.LAST_WEEK)
+    return (
+        <Box className={styles.container}>
+            <Flex justifyContent="flex-end">
+                <Menu>
+                    <MenuButton as={Button} rightIcon={<AiFillCaretDown />} w="10rem">
+                        {duration}
+                    </MenuButton>
+                    <MenuList>
+                        {Object.keys(Duration).map((key, index) => <MenuItem key={index} onClick={() => setDuration(Duration[key])}>{Duration[key]}</MenuItem>)}
+                    </MenuList>
+                </Menu>
+            </Flex>
+        </Box>
+    )
 }
 
 Dashboard.requireAuth = true;
